dacbbb2600784c7bd4e59057e481e3794f727e86
antdkhtr <antd3149@gmail.com>
2021-06-07 16:35:19 +0300

    skills block add

--- src/skillsBlock/SkillsBlock.js
+++ src/skillsBlock/SkillsBlock.js
@@ -1,20 +1,51 @@
 import style from './SkillsBlock.module.css'
 import containStyle from '../common/styles/Contanier.module.css'
-import { Skill } from './skill/Skill'
+import {Skill} from './skill/Skill'
 import {Title} from "../common/components/title/Title";
+import { faReact } from '@fortawesome/free-brands-svg-icons'
+import {faJs} from '@fortawesome/free-brands-svg-icons'
+import {faCss3Alt} from '@fortawesome/free-brands-svg-icons'
+import redux from '../assets/image/redux.svg'
+import typeScript from '../assets/image/ts.svg'
+import {faHtml5} from '@fortawesome/free-brands-svg-icons'
 
 export function SkillsBlock() {
+
+    let icons = {
+        react:faReact,
+        javaScript:faJs,
+        css:faCss3Alt,
+        redux:redux,
+        typeScript:typeScript,
+        faHtml5:faHtml5
+    }
+
+
+
+
+
     return (
         <div className={style.skillsBlock}>
             <div className={`${containStyle.container} ${style.skillsContainer}`}>
 
                 <Title text={"Skills"} firstColorText={"My "}/>
-                
+
                 <div className={style.skills}>
-                    <Skill title={"JavaScript"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
-                    <Skill title={"CSS"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
-                    <Skill title={"React"} description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"} />
-                     </div>
+                    <Skill title={"JavaScript"} fortawesomeIcon={icons.javaScript}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill title={"CSS"} fortawesomeIcon={icons.css}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill title={"React"} fortawesomeIcon={icons.react}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                </div>
+                <div className={style.skills}>
+                    <Skill title={"TypeScript"} svgIcon={icons.typeScript}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill title={"Redux"} svgIcon={icons.redux}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill title={"HTML"} fortawesomeIcon={icons.faHtml5}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                </div>
             </div>
         </div>
     );
--- src/skillsBlock/skill/Skill.js
+++ src/skillsBlock/skill/Skill.js
@@ -1,13 +1,19 @@
 import style from './Skill.module.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export function Skill(props) {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
-            <h3>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.icon}>
+                {props.fortawesomeIcon
+                    ?
+                    <FontAwesomeIcon size={"4x"} color={"color:#c32865"} icon={props.fortawesomeIcon}/>
+                    :
+                    <img src={props.svgIcon}/>}
+            </div>
+            <h3 className={style.skillTitle}>{props.title}</h3>
+            <span className={style.skillDescription}>{props.description}</span>
         </div>
     );
 }
