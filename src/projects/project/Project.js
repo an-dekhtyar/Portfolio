937f87d6412696487df4a1ca3430b7dc58c7fc9b
antdkhtr <antd3149@gmail.com>
2021-07-22 19:30:32 +0300

    fix project block

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -4,11 +4,13 @@ import style from './Project.module.scss'
 export function Project(props) {
     return (
         <div className={style.project}>
+            <a href={''}>
             <div className={style.viewBlock} style={props.style}>
-                <a href={''}>
-                    {/*  <button className={style.button}>View Project</button> */}
-                </a>
+
+                      {/*<button className={style.button}>View</button>*/}
+
             </div>
+            </a>
             <div className={style.descriptionBlock}>
                 <h3 className={style.title}>{props.title}</h3>
                 <div className={style.description}>{props.description}</div>
